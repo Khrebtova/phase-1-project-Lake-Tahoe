1bc9b18ac781c4471adbe2bebdfa344b254aff56
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-01-12 15:22:05 -0800

    started build index.JS file with DOM content loaded event, Get request from Json server

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 /**DOM content Loaded */
-
+document.addEventListener("DOMContentLoaded", () => {
+    getAllActivities()
+})
 
 /** GlobalVariables */
-
+let seasonSelect = document.getElementById("season")
+let selectedActivityId = 1
+let selectedActivity ;
+let listOfAllActvities = []
 
 /** Node getters */
-
+const detailsDiv = () => document.getElementById('details')
+const menuDiv = () => document.getElementById("activity-menu")
+const filter = () => document.getElementById("sort")
+const commentForm = () => document.getElementById("comment-form")
 
 /**Event Listeners */
 
 
 /** Event Handlers */
+const getAllActivities = () => {
+    resetMenuDiv()
+    fetch(`http://localhost:3000/activities`)
+    .then(res => res.json())
+    .then(data => { 
+        listOfAllActvities = data;
+        data.forEach(el => renderPicture(el))
+        handleBigPic(data[0])
+    })
+} 
+
+const renderPicture = (activity) => {
+   console.log(activity.name)
+}
+
+const handleBigPic = (activity) => {
+    console.log(activity.name, "will be first big picture")
+}
 
 
 /** MISC */
